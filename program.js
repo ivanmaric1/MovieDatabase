bb6f0f951fcb23e0002f83bddf239f7b2fbe91f4
Ivan <js314head@gmail.com>
2020-04-07 16:22:04 +0200

    Program dovršen

--- program.js
+++ program.js
@@ -1,3 +1,100 @@
+// Model za izradu knjige
+class Movie {
+    constructor(name,url,rating) {
+        this.name = name,
+        this.url = url,
+        this.rating = rating
+    }
+}
+
+class UI {
+    static displayMovies() {
+        let movies = Storage.getMovies()
+        if(movies === null || movies.length < 1) {
+            document.querySelector('.movieDatabase').style.display = 'none'
+        } else {
+            document.querySelector('.noMovie').style.display = 'none'
+            movies.forEach((item) => {
+                let bookItem = document.createElement('li')
+                bookItem.innerHTML = `
+                    <div>
+                        <img src='${item.url}' alt='${item.name}' class='list__img'>
+                    </div>
+                    <div class='list__description'>
+                        <h2 class='list__name'>${item.name}</h2>
+                        <p class='list__rating'>${item.rating}/5</p>
+                        <button class='delete'>OBRIŠI</button>
+                    </div>
+                `
+                document.querySelector('.movieDatabase__list').appendChild(bookItem)
+            })
+        }
+    }
+
+    static openOverlay() {
+        document.querySelector('.overlay').style.display = 'block'
+    }
+
+    static createMovie() {
+        let name = document.querySelector('.name').value
+        let url = document.querySelector('.url').value
+        let rating = document.querySelector('.rating').value
+        if(name.trim() === '' || url.trim() === '' || rating.trim() === '' || rating < 1 || rating > 5 ) {
+            alert('Molimo ispunite sva polja ispravno. Rating od 1 do 5!')
+        }
+        let newBook = new Movie(name,url,rating)
+        return newBook
+    }
+    
+    static removeMovie(e) {
+        let deleted = e.target.parentElement.parentElement.lastElementChild.firstElementChild.innerHTML
+        e.target.parentElement.parentElement.remove()
+        Storage.deleteMovie(deleted)
+        }
+    }
+
+class Storage {
+    static getMovies() {
+        let movies;
+        if(localStorage.getItem('movies') === null) {
+            movies = []
+        } else {
+            movies = JSON.parse(localStorage.getItem('movies'))
+        }
+        return movies
+    }
+
+    static addMovie(e) {
+        let movies = Storage.getMovies()
+        let newMovie = UI.createMovie()
+        movies.push(newMovie)
+        localStorage.setItem('movies',JSON.stringify(movies))
+    }
+
+    static deleteMovie(item) {
+        let movies = Storage.getMovies()
+        
+        movies.forEach((i,index) => {
+            if(item === i.name) {
+                movies.splice(index, 1)
+            }
+        })
+        localStorage.setItem('movies', JSON.stringify(movies))
+    }
+}
+
+// Event - DOM učitavanje
+document.addEventListener('DOMContentLoaded', UI.displayMovies)
+
+// Eventi - otvaranje forme
+document.querySelector('.btn').addEventListener('click', UI.openOverlay)
+document.querySelector('.btn-newEntry').addEventListener('click', UI.openOverlay)
+
+// Event - kreiranje knjige
+document.querySelector('.btn--save').addEventListener('click', Storage.addMovie)
+
+// Event - brisanje knjige
+document.querySelector('.movieDatabase__list').addEventListener('click', UI.removeMovie)
 
 
 
@@ -5,11 +102,14 @@
 
 
 
-// let damidata = [
-//     {ime: 'Nova zemlja', slika: 'https://shop.skolskaknjiga.hr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/3/137681.jpg', rating: 3},
-//     {ime: 'Ispričat ću ti pirču', slika: 'https://shop.skolskaknjiga.hr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/3/130566.jpg',rating: 5},
-//     {ime: 'Umijeće ratovanja', slika: 'https://mozaik-knjiga.hr/wp-content/uploads/archive/images/9789531402521.jpg',rating: 5},
-//     {ime: '50 nijansi sive', slika: 'https://www.tportal.hr/media/thumbnail/w1000/61584.jpeg',rating: 2},
-//     {ime: 'Dječja bibilija', slika: 'https://verbum.hr/media/catalog/product/cache/6b552494e4d4a60152e5cee20c61d11a/1/4/146.jpg',rating: 5},
-//     {ime: 'Ako se ikada sretnemo', slika: 'https://miss7.24sata.hr/media/img/b3/72/89d78827c7aafa090f99.jpeg',rating: 5},
-// ]
+
+
+
+
+
+
+
+
+
+
+
