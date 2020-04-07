0753cfd22cb0c22fc4f88b0f63e81f13072b96c3
Ivan <js314head@gmail.com>
2020-04-07 11:04:49 +0200

    Organiziran css

--- program.js
+++ program.js
@@ -1,7 +1,15 @@
-document.querySelector('.gumb--upis').addEventListener('click', function() {
-    document.querySelector('.forma').classList.toggle('visible')
-})
 
-document.querySelector('.gumb--poništi').addEventListener('click', function() {
-    document.querySelector('.forma').classList.toggle('visible')
-})
+
+
+
+
+
+
+// let damidata = [
+//     {ime: 'Nova zemlja', slika: 'https://shop.skolskaknjiga.hr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/3/137681.jpg', rating: 3},
+//     {ime: 'Ispričat ću ti pirču', slika: 'https://shop.skolskaknjiga.hr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/3/130566.jpg',rating: 5},
+//     {ime: 'Umijeće ratovanja', slika: 'https://mozaik-knjiga.hr/wp-content/uploads/archive/images/9789531402521.jpg',rating: 5},
+//     {ime: '50 nijansi sive', slika: 'https://www.tportal.hr/media/thumbnail/w1000/61584.jpeg',rating: 2},
+//     {ime: 'Dječja bibilija', slika: 'https://verbum.hr/media/catalog/product/cache/6b552494e4d4a60152e5cee20c61d11a/1/4/146.jpg',rating: 5},
+//     {ime: 'Ako se ikada sretnemo', slika: 'https://miss7.24sata.hr/media/img/b3/72/89d78827c7aafa090f99.jpeg',rating: 5},
+// ]
